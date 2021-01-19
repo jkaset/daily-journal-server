00751dc8706232fd7bdd85b0ee8ae0668f45c4a9
Jasmin Kaset <kasetski@gmail.com>
2021-01-19 10:44:45 -0600

    got single entry

--- entries/request.py
+++ entries/request.py
@@ -2,6 +2,7 @@ import sqlite3
 import json
 from models import Entry
 
+
 def get_all_entries():
     with sqlite3.connect("./dailyjournal.db") as conn:
 
@@ -29,3 +30,26 @@ def get_all_entries():
             entries.append(entry.__dict__)
 
     return json.dumps(entries)
+
+
+def get_single_entry(id):
+    with sqlite3.connect("./dailyjournal.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            e.id,
+            e.date,
+            e.concept,
+            e.entry,
+            e.mood_id
+        FROM entry e
+        WHERE e.id = ?
+        """, ( id, ))
+
+        data = db_cursor.fetchone()
+
+        entry = Entry(data['id'], data['date'], data['concept'], data['entry'], data['mood_id'])
+
+        return json.dumps(entry.__dict__)
